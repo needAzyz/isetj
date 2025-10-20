d6db26886c5cc3f69c4cc5315ce4329721b591fc
Azyz <contact.azyz@gmail.com>
2025-10-20 15:30:52 +0100

    lesson one isetj

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
+import { Container } from "./container/container";
+import { Signup } from "./signup/signup";
+import { Home } from "./home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Container, Signup, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
